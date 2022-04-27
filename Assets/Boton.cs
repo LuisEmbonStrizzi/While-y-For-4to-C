931882c7b1f760605717ea7e92fc19c7cdc0df40
LuisEmbonStrizzi <luisembonstrizzi@gmail.com>
2022-04-27 14:47:56 -0300

    Initial commit

---
+++ Assets/Boton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boton : MonoBehaviour
+{
+    public int entero1;
+    public int entero2;
+    
+    public void BtnPress()
+    {
+        if (entero2 >= entero1)
+        {
+            Debug.Log("Error");
+        }
+        else
+        {
+            while (entero2 < entero1)
+            {
+                Debug.Log(entero1);
+                entero1--;
+            }
+        }       
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
